d287b49ac4f15e2869567c4d3a0c572b68921914
Fernando Andrés Briones Fernandoi <fernandoandres.briones@alumnos.ulagos.cl>
2024-11-30 22:40:14 -0300

    Update

--- enemy.py
+++ enemy.py
@@ -1,41 +1,41 @@
-import pygame 
-import config 
-import math as mt 
+import pygame
+import config
+import math as mt
 
 
-class Enemy():
+class Enemy:
     def __init__(self):
         self.estatus = True
         self.life = 100
-    def draw(self):
+
+    def draw(self, screen):
         pass
 
     def death(self):
-        if self.life <= 0: 
+        if self.life <= 0:
             self.life = 0
-            self.estatus = False    
-            
+            self.estatus = False
+
     def move(self, to_x: int, to_y: int):
         self.ex = to_x - self.x
         self.ey = to_y - self.y
-        distance = mt.sqrt((self.ex - self.x)**2 + (self.ey - self.y)**2)  
+        distance = mt.sqrt((self.ex - self.x) ** 2 + (self.ey - self.y) ** 2)
         self.enemy_shape.center = (self.x, self.y)
 
-        if distance != 0: 
+        if distance != 0:
             self.ex /= distance
             self.ey /= distance
         self.x += self.ex * self.speed
         self.y += self.ey * self.speed
         self.animate()
 
-
-    def colision(self,player, enemies):
+    def colision(self, player, enemies):
         for enemy in enemies:
-                if player.pj_shape.colliderect(enemy.enemy_shape):  
-                    player.takeDamage(self.AD)
+            if player.pj_shape.colliderect(enemy.enemy_shape):
+                player.takeDamage(self.AD)
+
     def animate(self):
         pass
-
 
 
 class Enemy_orco(Enemy):
@@ -47,37 +47,37 @@ class Enemy_orco(Enemy):
         self.color = (255, 0, 0)
         self.AD = 12
 
-        self.enemy_shape = pygame.Rect(0, 0 , 50 , 60)
-        self.enemy_shape.center = (self.x , self.y)
+        self.enemy_shape = pygame.Rect(0, 0, 50, 60)
+        self.enemy_shape.center = (self.x, self.y)
         self.speed = 1
-        self.animations= config.orco
+        self.animations = config.orco
         self.frame = 0
         self.image = self.animations[self.frame]
-        self.ticks = self.ticks = pygame.time.get_ticks()
-
-
+        self.ticks = pygame.time.get_ticks()
 
     def animate(self):
-            colldown_ani =  150 #ms 
-            self.image = self.animations[self.frame]
-            
-            if pygame.time.get_ticks() - self.ticks >= colldown_ani:
-                self.frame += 1
-                self.ticks = pygame.time.get_ticks()
-            if self.frame >= len(self.animations):
-                self.frame = 0
-                
-            
+        colldown_ani = 150  # ms
+        self.image = self.animations[self.frame]
+
+        if pygame.time.get_ticks() - self.ticks >= colldown_ani:
+            self.frame += 1
+            self.ticks = pygame.time.get_ticks()
+        if self.frame >= len(self.animations):
+            self.frame = 0
+
     def draw(self, screen):
         self.enemy_shape.topleft = (self.x, self.y)
-       #pygame.draw.rect(screen, self.color, self.enemy_shape, 1)
-        screen.blit(self.image , self.enemy_shape.topleft)
 
+        # Dibujar sombra
+        shadow_rect = pygame.Rect(0, 0, 60, 20)  # Tamaño de la sombra
+        shadow_rect.center = (self.x, self.y + 35)  # Posición debajo del enemigo
+        pygame.draw.ellipse(screen, (0, 0, 0, 128), shadow_rect)  # Sombra translúcida
+
+        # Dibujar la imagen del enemigo
+        screen.blit(self.image, self.enemy_shape.topleft)
 
     def death(self):
         return super().death()
-    
-
 
 
 class Slime(Enemy):
@@ -93,24 +93,23 @@ class Slime(Enemy):
         self.AD = 5
         self.image = self.animations[self.frame]
         self.speed = 0.1
-        self.enemy_shape = pygame.Rect(0, 0 , 42 , 30)
-        self.enemy_shape.topleft = (self.x, self.y )
+        self.enemy_shape = pygame.Rect(0, 0, 42, 30)
+        self.enemy_shape.topleft = (self.x, self.y)
 
-
-  
-        
     def draw(self, screen):
         self.enemy_shape.center = (self.x, self.y)
-        #pygame.draw.rect(screen, (255,0,0), self.enemy_shape, 1)
+
+        # Dibujar sombra
+        shadow_rect = pygame.Rect(0, 0, 50, 15)  # Tamaño de la sombra
+        shadow_rect.center = (self.x, self.y + 20)  # Posición debajo del enemigo
+        pygame.draw.ellipse(screen, (0, 0, 0, 128), shadow_rect)  # Sombra translúcida
+
+        # Dibujar la imagen del enemigo
         screen.blit(self.image, self.image.get_rect(center=self.enemy_shape.center).topleft)
 
-
-
     def animate(self):
-        colldown_ani =  150 #ms 
+        colldown_ani = 150  # ms
         self.image = self.animations[self.frame]
-        
-
 
         if pygame.time.get_ticks() - self.ticks >= colldown_ani:
             self.frame += 1
@@ -122,8 +121,7 @@ class Slime(Enemy):
         return super().death()
 
 
-
-class boss(Enemy):
+class Boss(Enemy):
     def __init__(self):
         super().__init__()
         self.life = 100000
